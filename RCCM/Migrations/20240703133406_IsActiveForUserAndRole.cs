dcc9c70be5e4f60a9df54091f27e56e1e1226a53
ZIYAD MOHAMMED DAKHEL ALFAIDI <Y4M4110658@rcjy.edu.sa>
2024-07-03 17:16:04 +0300

    Added is active for the user and role tables

---
+++ RCCM/Migrations/20240703133406_IsActiveForUserAndRole.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RCCM.Migrations
+{
+    public partial class IsActiveForUserAndRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "User",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Role",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Role");
+        }
+    }
+}
