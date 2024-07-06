eae6c248a63f72ad7a33a687653731fd61f211bf
ZIYAD MOHAMMED DAKHEL ALFAIDI <Y4M4110658@rcjy.edu.sa>
2024-07-07 02:28:40 +0300

    Added CRUD operations for user table.

--- Core/Entities/ViewModel/RoleViewModel.cs
+++ Core/Entities/ViewModel/RoleViewModel.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.Entities.Model
+namespace Core.Entities.ViewModel
 {
-    public class Role
+    public class RoleViewModel
     {
-        [Key]
         public int RoleId { get; set; }
         public string RoleName { get; set; }
         public bool IsActive { get; set; }
-
-        //public List<User> Users { get; set; }
 
     }
 }
